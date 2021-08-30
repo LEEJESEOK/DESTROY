5464fdbadbf778f43ffb08c12ff31b7db3caf5ce
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-08-30 10:40:05 +0900

    플레이어 체력 추가
    - En_01, En_04, test_bullet isTrigger 속성 적용

--- Assets/Jeseok/Scripts/HP.cs
+++ Assets/Jeseok/Scripts/HP.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class HP : MonoBehaviour
 {
+    public int maxHP;
+    int currentHP;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHP = maxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Destroy(other.gameObject);
+            --currentHP;
+            print(currentHP);
+
+            if (currentHP <= 0)
+                Destroy(gameObject);
+        }
+    }
+
 }
