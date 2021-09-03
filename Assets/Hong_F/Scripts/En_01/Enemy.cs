cb13c3bedba6394e07b5d194e0a14d3bad837e4e
ThierryHenry2 <88121326+ThierryHenry2@users.noreply.github.com>
2021-09-03 15:35:34 +0900

    Fracture
    ddd

--- Assets/Hong_F/Scripts/En_01/Enemy.cs
+++ Assets/Hong_F/Scripts/En_01/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Project.Scripts.Fractures;
 
 public class Enemy : MonoBehaviour
 {
@@ -12,18 +13,37 @@ public class Enemy : MonoBehaviour
     GameObject player;
     // Start is called before the first frame update
 
-
+    int enemyHP = 3;
+    GameObject fractureObj;
     void Start()
     {
-         player = GameObject.Find("Player");
+        player = GameObject.Find("Player");
 
         nav = GetComponent<NavMeshAgent>();
-
+        //gameObject.GetComponent<FractureThis>().enabled = false;
+        fractureObj = FractureThis.GetInstance().CreateFracture(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         nav.SetDestination(player.transform.position);
+
+        DestoryEn_01();
+
+    }
+
+    public void DestoryEn_01()
+    {
+        if (enemyHP <= 0)
+        {
+            //gameObject.GetComponent<FractureThis>().enabled = true;
+            //gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+            Destroy(gameObject);
+            fractureObj.SetActive(true);
+
+            //fractureObj.transform.position = transform.position;
+
+        }
     }
 }
---
+++ Assets/Hong_F/Scripts/FractureThis.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Random = System.Random;
+
+namespace Project.Scripts.Fractures
+{
+    public class FractureThis
+    {
+        [SerializeField] private Anchor anchor = Anchor.None;
+        [SerializeField] private int chunks = 20;
+        [SerializeField] private float density = 50;
+        [SerializeField] private float internalStrength = 100;
+
+        [SerializeField] private Material insideMaterial;
+        [SerializeField] private Material outsideMaterial;
+
+        private Random rng = new Random();
+
+        public GameObject fractureObj;
+
+        private static FractureThis Instance;
+
+        public static FractureThis GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new FractureThis();
+            }
+
+            return Instance;
+        }
+
+        public GameObject CreateFracture(GameObject gameObject)
+        {
+            GameObject fractureObj = FractureGameobject(gameObject).fractureObj;
+            fractureObj.SetActive(false);
+            return fractureObj;
+        }
+
+        public ChunkGraphManager FractureGameobject(GameObject gameObject)
+        {
+            var seed = rng.Next();
+            return Fracture.FractureGameObject(
+                gameObject,
+                anchor,
+                seed,
+                chunks,
+                insideMaterial,
+                outsideMaterial,
+                internalStrength,
+                density
+            );
+        }
+    }
+}
