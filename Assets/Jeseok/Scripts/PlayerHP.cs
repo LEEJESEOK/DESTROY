a879cc476682f1ac038fe7dfd9771f415893306f
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-09-10 14:47:12 +0900

    En_03 Beam 충돌효과 적용

--- Assets/Jeseok/Scripts/PlayerHP.cs
+++ Assets/Jeseok/Scripts/PlayerHP.cs
@@ -77,7 +77,11 @@ public class PlayerHP : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Destroy(other.gameObject);
+            if (other.gameObject.name.Contains("BeamBullet"))
+                other.gameObject.SetActive(false);
+            else
+                Destroy(other.gameObject);
+                
             //TODO 데미지 수치 적용
             --currentHP;
 
