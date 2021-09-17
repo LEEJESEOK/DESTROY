801afe44d5fcb1def752cff3f5329b3232fd387d
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-09-17 14:04:03 +0900

    랭킹보드 구현
    Former-commit-id: b6112f2d8e3fe2ad14fac8449f7bdf44cdc83c4d

--- Assets/Jeseok/Scripts/Manager/GameoverManager.cs
+++ Assets/Jeseok/Scripts/Manager/GameoverManager.cs
@@ -4,14 +4,72 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+using Firebase;
+using Firebase.Database;
+
+using System.Linq;
+
 public class GameoverManager : MonoBehaviour
 {
+    public class Record
+    {
+        public string userID;
+        public int score;
+        public string date;
+
+        public Record()
+        {
+            this.userID = SystemInfo.deviceUniqueIdentifier;
+            this.score = 0;
+            this.date = "";
+        }
+
+        public Record(int score, string date)
+        {
+            userID = SystemInfo.deviceUniqueIdentifier;
+            this.score = score;
+            this.date = date;
+        }
+
+        public Record(string userID, int score, string date)
+        {
+            this.userID = userID;
+            this.score = score;
+            this.date = date;
+        }
+
+        public Dictionary<string, System.Object> ToDictionary()
+        {
+            Dictionary<string, System.Object> result = new Dictionary<string, System.Object>();
+            result["userID"] = this.userID;
+            result["score"] = this.score;
+            result["date"] = this.date;
+
+            return result;
+        }
+    }
+
     public static GameoverManager instance;
 
 
     AudioSource button;
 
-    public Text scoreText;
+    public Text myScoreText;
+    public Text dateText;
+
+    public GameObject scoreUIObj;
+    int gameScore;
+    string gameDate;
+
+    public GameObject rankingboard;
+    List<Record> records = new List<Record>();
+    public int rankingSize = 3;
+
+    public GameObject refreshButton;
+
+
+    DatabaseReference reference;
+    bool scoreLoadFlag = false;
 
 
     private void Awake()
@@ -29,13 +87,28 @@ public class GameoverManager : MonoBehaviour
     {
         button = GetComponent<AudioSource>();
 
-        scoreText.text = "" + ScoreManager.instance.currentScore;
+        if (ScoreManager.instance != null)
+            gameScore = ScoreManager.instance.currentScore;
+        gameDate = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+
+        myScoreText.text = gameScore.ToString();
+        dateText.text = gameDate;
+
+
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        if (ScoreManager.instance != null)
+            RankingUpload();
+
+
+        RankingLoad();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-
+        if (scoreLoadFlag == true)
+        {
+            UpdateRankingboard();
+        }
     }
 
     public void onClickRetry()
@@ -48,5 +121,112 @@ public class GameoverManager : MonoBehaviour
     {
         button.Play();
         Application.Quit();
+    }
+
+    public void onClickRefresh()
+    {
+        RankingLoad();
+        refreshButton.SetActive(false);
+    }
+
+    void RankingUpload()
+    {
+        refreshButton.SetActive(true);
+
+        Record currentRecord = new Record(gameScore, gameDate);
+        string json = JsonUtility.ToJson(currentRecord);
+
+        Record pastRecord = new Record();
+        // 본인 점수를 불러옴
+        reference.Child("high_score").Child(currentRecord.userID).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                print("error");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshots = task.Result;
+
+                // snapshot의 자식 개수
+                print(snapshots.ChildrenCount);
+
+                foreach (DataSnapshot snapshot in snapshots.Children)
+                {
+                    IDictionary record = (IDictionary)snapshot.Value;
+
+                    pastRecord = new Record(int.Parse(record["score"].ToString()), record["date"].ToString());
+                }
+
+                scoreLoadFlag = true;
+            }
+        });
+
+
+        // 현재점수와 비교해서 더 높으면 업데이트
+        // reference.Child("high_score").Child(System.DateTime.Now.ToString()).SetRawJsonValueAsync(json);
+        // reference.Child("high_score").Child(SystemInfo.deviceUniqueIdentifier).SetRawJsonValueAsync(json);
+        if (gameScore > pastRecord.score)
+            WriteNewScore(currentRecord);
+    }
+
+    void WriteNewScore(Record record)
+    {
+        string key = reference.Child("high_score").Child(record.userID).Push().Key;
+
+        Dictionary<string, System.Object> recordValues = record.ToDictionary();
+
+        Dictionary<string, System.Object> childUpdates = new Dictionary<string, System.Object>();
+
+        childUpdates["/high_score/" + record.userID] = recordValues;
+        reference.UpdateChildrenAsync(childUpdates);
+    }
+
+    public void RankingLoad()
+    {
+        reference.Child("high_score").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                print("error");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshots = task.Result;
+
+                // snapshot의 자식 개수
+                print(snapshots.ChildrenCount);
+
+                foreach (DataSnapshot snapshot in snapshots.Children)
+                {
+                    IDictionary record = (IDictionary)snapshot.Value;
+                    records.Add(new Record(record["userID"].ToString(), int.Parse(record["score"].ToString()), record["date"].ToString()));
+                }
+
+                scoreLoadFlag = true;
+            }
+
+
+        });
+    }
+
+    void UpdateRankingboard()
+    {
+        scoreLoadFlag = false;
+
+        List<Record> sorted = records.OrderByDescending(record => record.score).ToList();
+
+        for (int i = 0; i < rankingSize && i < sorted.Count; ++i)
+        {
+            GameObject scoreUI = Instantiate(scoreUIObj);
+            scoreUI.transform.SetParent(rankingboard.transform);
+            SetScoreUI(scoreUI, sorted[i].score, sorted[i].date);
+        }
+    }
+
+    void SetScoreUI(GameObject scoreUI, int score, string date)
+    {
+        scoreUI.transform.Find("Score").GetComponent<Text>().text = score.ToString();
+        scoreUI.transform.Find("Date").GetComponent<Text>().text = date;
     }
 }
